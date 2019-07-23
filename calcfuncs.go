97099c3e088292da6fef6c6a928263327a830152
notched-mahogany <52700507+notched-mahogany@users.noreply.github.com>
2019-07-23 09:52:34 +0100

    Concept Update (buggy)
    Just a concept on how I will fix the for loops

--- calcfuncs.go
+++ calcfuncs.go
@@ -1,12 +1,43 @@
-//This is a basic calculator. It can only run with two numbers currently
+//This is a basic calculator. It can only run addition, subtraction, division and multiplication
+//Current bugs:
+//	"For loops being go
+//	"Splice issues with the count I'm calling being outside the index range  
 package main
+
 
 import (
 	"fmt"
 )
 
+func fix() {
+	fmt.Print("\nWhat number are you adding?\n>>")
+	fmt.Scanln(&b)
+	lst = append(lst, b)
+	swing+=1
+}
+
+func selection(){
+	var y = int(lst[count]-1)
+		if choice == "A"{
+			x := x+y
+			_=x
+		}
+		if choice == "S"{
+			x := x-y
+			_=x
+		}
+		if choice == "D"{
+			x := x/y
+			_=x
+		}
+		if choice == "M"{
+			x := x*y
+			_=x
+		}
+		count+=1
+}
+
 func main() {
-	//ask what operator to use
 	fmt.Print("*************Calculator*************\n\nWhat operator would you like to use?\n\nAdd (input 'A')\nSubtract (input 'S')\nMultiply (input 'M')\nDivide (input 'D')\n\nTo Quit press 'Q'\n>>")
 	var choice string
 	fmt.Scanln(&choice)
@@ -21,34 +52,13 @@ func main() {
 		fmt.Scan(&b)
 		lst = append(lst, b)
 		for ; swing <= number-1 ;{
-			fmt.Print("What number are you adding?\n>>")
-			fmt.Scanln(&b)
-			lst = append(lst, b)
-			swing+=1
+			fix()
 		}
 		var x = int(lst[0])
 		var count int = 1
-		for ; count <= len(lst); {
-			var y = int(lst[count]-1)
-			if choice == "A"{
-				x := x+y
-				_=x
-			}
-			if choice == "S"{
-				x := x-y
-				_=x
-			}
-			if choice == "D"{
-				x := x/y
-				_=x
-			}
-			if choice == "M"{
-				x := x*y
-				_=x
-			}
-			count+=1
+		for ; count <= swing; {
+			selection()
 		}
 		fmt.Print("The inputed values work out to ",x)
 	}
 }
-
